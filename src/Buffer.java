ff30803a501efffb2008052151d2b240186ff40c
Lars Nysom <LANY@ucn.dk>
2021-02-19 16:18:40 +0100

    Reset demo

--- src/Buffer.java
+++ src/Buffer.java
@@ -4,15 +4,15 @@ public class Buffer {
 	private int value = 0;
 
 	public void setValue(int value) {
-
-		System.out.println(Thread.currentThread().getName() + " thread sets value " + value);
-
+				
 		this.value = value;
+
+		System.out.println(Thread.currentThread().getName() + " thread sets value " + this.value);
 	}
 
 	public int getValue() {
-
-		System.out.println(Thread.currentThread().getName() + " thread reads value " + value);
+				
+		System.out.println(Thread.currentThread().getName() + " thread reads value " + this.value);
 
 		return this.value;
 	}
