c7f63a866b7a1046464ac5a8f728f096f19c85f7
Lars Nysom <LANY@ucn.dk>
2021-03-04 08:21:13 +0100

    Removed bugs

--- src/Buffer.java
+++ src/Buffer.java
@@ -3,16 +3,20 @@ public class Buffer {
 
 	private int value = 0;
 
-	public void setValue(int value) throws InterruptedException {
-		Thread.sleep((long) (Math.random() * 3001));		
-		this.value = value;
-		System.out.println(Thread.currentThread().getName() + " thread sets value " + this.value);
+	public int setValue(int value) throws InterruptedException {
+		Thread.sleep((long) (Math.random() * 3001));
+		if (this.value == 0) {
+			this.value = value;
+			return this.value;
+		}
+		return 0;
 	}
 
 	public int getValue() throws InterruptedException {
 		Thread.sleep((long) (Math.random() * 3001));
-		System.out.println(Thread.currentThread().getName() + " thread reads value " + this.value);
-		return this.value;
+		int res = this.value;
+		this.value = 0;
+		return res;
 	}
 
 }
